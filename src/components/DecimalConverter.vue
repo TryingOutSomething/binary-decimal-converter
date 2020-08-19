e6dcf66030356d605eb2073de8d97ea1352c4a94
Jan Owyeong <43298887+TryingOutSomething@users.noreply.github.com>
2020-08-19 15:01:41 +0800

    Initial commit

---
+++ src/components/DecimalConverter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input v-model="userInput">
+    <button @click="convertBinaryToDecimal">Convert to decimal</button>
+    <p v-if="showResult">{{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      userInput: "",
+      result: 0,
+      showResult: false
+    };
+  },
+
+  methods: {
+    convertBinaryToDecimal() {
+      this.result = 0;
+      const radix = 10;
+      if (this.userInput.length > 8) {
+        window.alert("Length cannot be more than 8");
+        return;
+      }
+      let base = 1;
+      for (let i = this.userInput.length - 1; i >= 0; i--) {
+        this.result += parseInt(this.userInput[i], radix) * base;
+        base *= 2;
+      }
+      this.showResult = true;
+    }
+  }
+};
+</script>
